03f269afbe0200cc1f76a0689d84848ad9bbdc86
zyongan <yonganzhong@outlook.com>
2019-11-21 15:12:05 +0800

    delete invalid code blank

--- ResNet/resnet_50.py
+++ ResNet/resnet_50.py
@@ -118,7 +118,10 @@ def ResNet50(include_top=True, weights='imagenet',
     """
 
     # Determine proper input shape
-
+    if not K.is_keras_tensor(input_tensor):
+        img_input = Input(tensor=input_tensor, shape=input_shape)
+    else:
+        img_input = input_tensor
 
     if K.image_data_format() == 'channels_last':
         bn_axis = 3
@@ -171,36 +174,4 @@ def ResNet50(include_top=True, weights='imagenet',
     # Create model.
     model = Model(inputs, x, name='resnet50')
 
-    # load weights
-    if weights == 'imagenet':
-        if include_top:
-            weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels.h5',
-                                    WEIGHTS_PATH,
-                                    cache_subdir='models',
-                                    md5_hash='a7b3fe01876f51b976af0dea6bc144eb')
-        else:
-            weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                                    WEIGHTS_PATH_NO_TOP,
-                                    cache_subdir='models',
-                                    md5_hash='a268eb855778b3df3c7506639542a6af')
-        model.load_weights(weights_path)
-        if K.backend() == 'theano':
-            layer_utils.convert_all_kernels_in_model(model)
-
-        if K.image_data_format() == 'channels_first':
-            if include_top:
-                maxpool = model.get_layer(name='avg_pool')
-                shape = maxpool.output_shape[1:]
-                dense = model.get_layer(name='fc1000')
-                layer_utils.convert_dense_weights_data_format(dense, shape, 'channels_first')
-
-            if K.backend() == 'tensorflow':
-                warnings.warn('You are using the TensorFlow backend, yet you '
-                              'are using the Theano '
-                              'image data format convention '
-                              '(`image_data_format="channels_first"`). '
-                              'For best performance, set '
-                              '`image_data_format="channels_last"` in '
-                              'your Keras config '
-                              'at ~/.keras/keras.json.')
     return model

d31818440846404b8dd43a2045fac7182f8b227c
alexchung <yonganzhong@outlook.com>
2020-03-04 15:15:21 +0800

    construct imdb classify network

--- text_vectorization/word_embedding.py
+++ text_vectorization/word_embedding.py
@@ -11,9 +11,12 @@
 #-------------------------------------------------------
 
 import os
-from keras.layers import Embedding
+import numpy as np
 from keras.datasets import imdb
 from keras import preprocessing
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Embedding
 
 def convert_index_to_word(index_list, index_dict):
     """
@@ -26,13 +29,11 @@ def convert_index_to_word(index_list, index_dict):
     return ' '.join([index_dict.get(index, '?') for  index in index_list])
 
 
-
 if __name__ == "__main__":
 
 
-    max_featurte = 1000
+    max_featurte = 10000
     max_len = 20
-
 
     # step 1 imdb dataset preprocessing
     (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_featurte)
@@ -46,13 +47,40 @@ if __name__ == "__main__":
     imdb_index['STARD'] = 1 # for start
     imdb_index['UNK'] = 2 # for UNKNOWN
     imdb_index['UNSET'] = 3
-
     imdb_dict = dict([(index, word) for word, index in imdb_index.items()])
-
-    # print(imdb_dict)
     # print(convert_index_to_word(index_dict=imdb_dict, index_list=x_train[0]))
+
     x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=max_len, padding='post', value=imdb_index['<PAD>'])
     x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=max_len, padding='post', value=imdb_index['<PAD>'])
+    print(x_train.shape) # (25000, 20)
+
+    # step 2 construct network
+    vocab_size = 10000
+    model = Sequential()
+    # (samples, max_len)->(25000, 20)
+    # Embedding 理解为字典，将证书索引，映射为密集向量
+    model.add(Embedding(input_dim=vocab_size, output_dim=8, input_length=max_len))
+    #(samples, max_len, output_dim)->(25000, 20, 8)
+    model.add(Flatten())
+    # (samples, max_len*output_dim)->(2500, 160)
+    model.add(Dense(1, activation='sigmoid'))
+    # (samples,1) ->(25000, 1)
+    model.summary()
+
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+
+    history = model.fit(x=x_train, y=y_train,
+                        epochs=10,
+                        batch_size=32,
+                        validation_split=0.2)
+
+
+
+
+
+
+
+
 
 
 
